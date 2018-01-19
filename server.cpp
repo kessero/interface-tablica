c8fc04ca1baf2cf31f35af7d5a17ea62b5a234f6
kesser <dkesselring@gmail.com>
2018-01-19 20:12:11 +0100

    poprawki edycyjne

--- server.cpp
+++ server.cpp
@@ -35,38 +35,37 @@ int main(){
 
   json config;
   config = read_configuration();
-int sockfd, newsockfd, port_number, pid;
-     socklen_t clilen;
-     struct sockaddr_in serv_addr, cli_addr;
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     port_number = config["SOCKETport"];
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(port_number);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0)
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     while (1) {
-         newsockfd = accept(sockfd,
-               (struct sockaddr *) &cli_addr, &clilen);
-         if (newsockfd < 0)
-             error("ERROR on accept");
-         pid = fork();
-         if (pid < 0)
-             error("ERROR on fork");
-         if (pid == 0)  {
-             close(sockfd);
-              Prepare_Data p_data(config["USBconverter"], config["rs485speed"], newsockfd);
-             exit(0);
-         }
-         else close(newsockfd);
+  int sockfd, newsockfd, port_number, pid;
+  socklen_t clilen;
+  struct sockaddr_in serv_addr, cli_addr;
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+    error("ERROR opening socket");
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  port_number = config["SOCKETport"];
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(port_number);
+  if (bind(sockfd, (struct sockaddr *) &serv_addr,
+    sizeof(serv_addr)) < 0)
+    error("ERROR on binding");
+   listen(sockfd,5);
+   clilen = sizeof(cli_addr);
+   while (1) {
+     newsockfd = accept(sockfd,
+     (struct sockaddr *) &cli_addr, &clilen);
+     if (newsockfd < 0)
+       error("ERROR on accept");
+     pid = fork();
+     if (pid < 0)
+       error("ERROR on fork");
+     if (pid == 0)  {
+       close(sockfd);
+       Prepare_Data p_data(config["USBconverter"], config["rs485speed"], newsockfd);
+       exit(0);
+       }
+     else close(newsockfd);
      } /* end of while */
-     close(sockfd);
-
+  close(sockfd);
   return 0;
 }
